f5f5431f2251ebbbaf55ecc133f7c40dbcda688e
Lee Salminen <leesalminen@gmail.com>
2023-10-18 15:15:01 -0600

    remove comment

--- app/lnurlp/[username]/callback/route.ts
+++ app/lnurlp/[username]/callback/route.ts
@@ -73,7 +73,6 @@ export async function GET(
   request: Request,
   { params }: { params: { username: string } },
 ) {
-  console.log(NOSTR_PUBKEY)
 
   const { searchParams } = new URL(request.url)
 
